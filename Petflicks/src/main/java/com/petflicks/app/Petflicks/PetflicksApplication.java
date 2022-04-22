4ed090b1275f39626f093e14cc17715c92a742c5
wilderchris <94183821+wilderchris@users.noreply.github.com>
2022-04-22 11:44:10 -0400

    spring initial push

---
+++ Petflicks/src/main/java/com/petflicks/app/Petflicks/PetflicksApplication.java
@@ -0,0 +1,13 @@
+package com.petflicks.app.Petflicks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetflicksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetflicksApplication.class, args);
+	}
+
+}
---
+++ Petflicks/src/test/java/com/petflicks/app/Petflicks/PetflicksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petflicks.app.Petflicks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetflicksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
