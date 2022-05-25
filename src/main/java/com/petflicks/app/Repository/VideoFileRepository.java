ff59a08fc199767679c51b9f4db5168c24d2dd86
wilderchris <94183821+wilderchris@users.noreply.github.com>
2022-05-25 12:28:49 -0500

    updated to use dotenv

--- src/main/java/com/petflicks/app/Repository/VideoFileRepository.java
+++ src/main/java/com/petflicks/app/Repository/VideoFileRepository.java
@@ -1,6 +1,16 @@
 package com.petflicks.app.Repository;
 
-public class VideoFileRepository {
+import io.github.cdimascio.dotenv.Dotenv;
 
-	
+public class VideoFileRepository {
+	Dotenv dotenv = Dotenv.configure()
+	        .directory("./src/main/resources")
+	        .ignoreIfMalformed()
+	        .ignoreIfMissing()
+	        .load();
+	final String ACCESS = dotenv.get("MY_ENV_ACCESS");
+	final String SECRET = dotenv.get("MY_ENV_SECRET");
+
+
+	System.out.println(ACCESS + "    " + SECRET);
 }
