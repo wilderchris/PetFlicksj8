f8b3fe81ab6e52ee18528996521ae79160372fda
wilderchris <94183821+wilderchris@users.noreply.github.com>
2022-06-05 13:36:42 -0500

    n
    adjustments for s3 methods test

--- src/main/java/com/petflicks/app/Repository/VideoFileRepository.java
+++ src/main/java/com/petflicks/app/Repository/VideoFileRepository.java
@@ -1,11 +1,48 @@
 package com.petflicks.app.Repository;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.WritableResource;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class VideoFileRepository {
 	Dotenv dotenv = Dotenv.configure().directory("./src/main/resources").filename("secrets.env").load();
-	
-//	System.out.println(dotenv.get("MY_ENV_ACCESS"));
 
-	
+	private String accessKey = dotenv.get("MY_ENV_ACCESS");
+	private String secretKey = dotenv.get("MY_ENV_SECRET");
+	private String awsRegion = dotenv.get("MY_REGION");
+	private String bucketName = "petflicksbucket";
+
+	@Autowired
+	ResourceLoader resourceLoader;
+
+	String s3Url = "s3://" + bucketName + "/";;// url syntax s3Url + "foldername"/"filename" in addition
+
+	public void downloadS3(String s3Url) throws IOException {
+		Resource resource = resourceLoader.getResource(s3Url);
+		File downloadedS3Object = new File(resource.getFilename());
+
+		try (InputStream inputStream = resource.getInputStream()) {
+			Files.copy(inputStream, downloadedS3Object.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		}
+	}
+
+	public void uploadS3(File file, String s3Url) throws IOException {
+		WritableResource resource = (WritableResource) resourceLoader.getResource(s3Url);
+
+		try (OutputStream outputStream = resource.getOutputStream()) {
+			Files.copy(file.toPath(), outputStream);
+		}
+	}
+
 }
